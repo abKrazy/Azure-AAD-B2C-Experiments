b2e8212b5d753d85372574fc165e0e5890a3b259
Alexandre Brisebois <brisebois@outlook.com>
2016-02-23 18:54:21 -0500

    Added oauth2-oidc-sdk-4.17

---
+++ Azure-AAD-B2C-Java/src/aad/b2c/AuthManager.java
@@ -0,0 +1,96 @@
+package aad.b2c;
+
+import com.nimbusds.jwt.SignedJWT;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URLEncoder;
+import java.text.ParseException;
+import java.util.Enumeration;
+
+public class AuthManager {
+
+    private static final String signinPolicy  = "B2C_1_sign-in";
+    private static final String signupPolicy  = "B2C_1_sign-up";
+    private static final String editprofilePolicy  = "B2C_1_edit-profile";
+
+    private final String aadUri;
+
+
+    public AuthManager(String tenant, String clientId, String redirectUri){
+
+        this.aadUri = "https://login.microsoftonline.com/"
+                    + tenant
+                    + ".onmicrosoft.com"
+                    + "/oauth2/v2.0/authorize?"
+                    + "client_id="
+                    + clientId
+                    + "&response_type=code+id_token"
+                    + "&redirect_uri="
+                    + URLEncoder.encode(redirectUri)
+                    + "&response_mode=form_post"
+                    + "&scope=openid%20offline_access"
+                    + "&state=arbitrary_data_you_can_receive_in_the_response"
+                    + "&nonce=12345"
+                    + "&p=";
+            }
+
+    private String id_token = "";
+    private String code = "";
+    private SignedJWT Jwt;
+
+    public void Load(HttpServletRequest request){
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            Enumeration paramNames = request.getParameterNames();
+
+            while(paramNames.hasMoreElements()) {
+                String paramName = (String)paramNames.nextElement();
+                if(paramName.equalsIgnoreCase("id_token")){
+                    id_token = request.getParameter(paramName);
+                }else if(paramName.equalsIgnoreCase("code")){
+                    code = request.getParameter(paramName);
+                }
+            }
+
+            if(!id_token.isEmpty()) {
+                try {
+                    Jwt = SignedJWT.parse(id_token);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public Boolean IsSignedSin()
+    {
+        return id_token != null && !id_token.isEmpty();
+    }
+
+    public String getSignInUri()
+    {
+        return this.aadUri + signinPolicy;
+    }
+    public String signupUri()
+    {
+        return this.aadUri + signupPolicy;
+    }
+    public String editProfileUri()
+    {
+        return this.aadUri + editprofilePolicy;
+    }
+
+    public String getJwtJson()
+    {
+        try {
+            if(Jwt == null){
+                return "";
+            }
+
+            return Jwt.getJWTClaimsSet().toJSONObject().toJSONString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+}
--- Azure-AAD-B2C-Java/web/index.jsp
+++ Azure-AAD-B2C-Java/web/index.jsp
@@ -1,3 +1,6 @@
+<%@ page import="aad.b2c.AuthManager" %>
+<%@ page import="java.util.Enumeration" %>
+
 <%--
   Created by IntelliJ IDEA.
   User: brise
@@ -6,11 +9,32 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <html>
   <head>
-    <title>$Title$</title>
+    <title>Hello Azure Active Directory</title>
   </head>
   <body>
-  $END$
+    <%
+      AuthManager manager = new AuthManager("alexandrebriseboisb2c","434c374e-d772-49e5-923e-26c870850098","http://localhost:8080/");
+      manager.Load(request);
+    %>
+    <div>
+     <%
+         Enumeration params = request.getParameterNames();
+         while(params.hasMoreElements()){
+             String paramName = (String)params.nextElement();
+             %>
+    <p><%=(paramName + " = " + request.getParameter(paramName))%></p>
+    <% } %>
+    </div>
+<div>
+    <%= manager.getJwtJson()%>
+</div>
+    <% if(manager.IsSignedSin()){ %>
+    Logged in
+    <% }else{ %>
+        <a href="<%=manager.getSignInUri()%>">Sign In</a>
+    <% }  %>
   </body>
 </html>
